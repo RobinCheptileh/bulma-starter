1ec9e0fcfb77fa60f1bdbc45430f25a0b59a72bb
Robin Cheptileh <robincheptileh@gmail.com>
2019-03-19 15:28:50 +0300

    make webpack environment aware and update readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,60 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true
-            }
-          }
+module.exports = (env, argv) => {
+    console.log(env);
+    console.log(argv);
+
+    let minimizeHTML = false;
+    const sassLoaders = ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'];
+
+    if (argv.mode === 'production') {
+        sassLoaders.push('postcss-loader');
+        minimizeHTML = true;
+    }
+
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                            options: {
+                                minimize: minimizeHTML
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(css|sass|scss)$/,
+                    use: sassLoaders
+                },
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'img'
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: './src/index.html',
+                filename: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
         ]
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          outputPath: 'img'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ]
+    };
 };
